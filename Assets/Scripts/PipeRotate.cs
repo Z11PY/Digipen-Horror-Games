faf0fc3b1757995697148c7d43f5f78d6eb44f7c
CorruptionDragon <thomasfulmer17@gmail.com>
2024-07-16 18:07:32 -0500

    Pipe Puzzle

---
+++ Assets/Scripts/PipeRotate.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+//Rotates the pipes
+
+public class PipeRotate : MonoBehaviour
+{
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            transform.Rotate(0, 0, 90);
+        }
+    }
+}
