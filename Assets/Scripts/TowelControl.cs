70b226a338ffe222df45029169b2a53b6b7f7b96
CorruptionDragon <thomasfulmer17@gmail.com>
2024-07-17 17:18:59 -0500

    Towel Puzzle

---
+++ Assets/Scripts/TowelControl.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+using TMPro;
+
+public class TowelControl : MonoBehaviour
+{
+    
+    void OnMouseDown()
+    {
+        //if (Event.isMouse == EventType.MouseDrag)
+        {
+            return;
+        }
+        Debug.Log("Mouse Dragged");
+    }
+
+    void Update()
+    {
+        
+    }
+}
