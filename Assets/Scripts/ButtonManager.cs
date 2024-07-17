244ed16690cc3c1f0469de6271aa9dc70ef2ecf9
doom99987 <dkonviss@outlook.com>
2024-07-17 16:17:31 -0700

    new floor2

--- Assets/Scripts/ButtonManager.cs
+++ Assets/Scripts/ButtonManager.cs
@@ -21,7 +21,7 @@ public class ButtonManager : MonoBehaviour
     /// </summary>
     public void StartGame()
     {
-        SceneManager.LoadScene("2ndFloor");
+        SceneManager.LoadScene("2ndFloor2");
     }
     /// <summary>
     /// goes to the main menu
@@ -36,7 +36,7 @@ public class ButtonManager : MonoBehaviour
     }
     public void RestartGame()
     {
-        SceneManager.LoadScene("2ndFloor");
+        SceneManager.LoadScene("2ndFloor2");
     }
     public void Credits()
     {
