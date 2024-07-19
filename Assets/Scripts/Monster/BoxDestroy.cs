c6e0e515563dfaf69ccb8cd5bb6a8fe9e81e0497
doom99987 <dkonviss@outlook.com>
2024-07-19 15:27:09 -0700

    box has music

--- Assets/Scripts/Monster/BoxDestroy.cs
+++ Assets/Scripts/Monster/BoxDestroy.cs
@@ -4,14 +4,22 @@ using UnityEngine;
 
 public class BoxDestroy : MonoBehaviour
 {
+    private AudioSource audioSource; // Audio source 
     void OnTriggerEnter2D(Collider2D other)
     {
         // Check if the triggered object has the tag "Box"
         if (other.gameObject.CompareTag("Monster"))
         {
+            // Play the audio clip once
+            if (audioSource != null && audioSource.clip != null)
+            {
+                audioSource.Play();
+            }
             // Destroy the box object
             Destroy(gameObject);
         }
+       
     }
+
 }
 
